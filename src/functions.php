68e11ac9cba0c12bdf351c8293d4ca95365c359a
jsguerra <jose@gcostudios.com>
2021-06-12 13:39:00 +0100

    updated version number

--- src/functions.php
+++ src/functions.php
@@ -3,8 +3,8 @@
  * Enqueue child styles.
  */
 function child_enqueue_styles() {
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array(), 1620160794 );
-	wp_enqueue_script( 'child-js', get_stylesheet_directory_uri() . '/js/script.js', array(), 1620160794 );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array(), 1623501512 );
+	wp_enqueue_script( 'child-js', get_stylesheet_directory_uri() . '/js/script.js', array(), 1623501512 );
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' ); // Remove the // from the beginning of this line if you want the child theme style.css file to load on the front end of your site.
